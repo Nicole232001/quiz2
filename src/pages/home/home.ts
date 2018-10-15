4d74495ce30f1918eb0990852edd41ffa8fabab3
Xenos141 <arturomr2001@hotmail.com>
2018-10-15 08:37:55 -0500

    subir archivo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { LamparaPage } from '../lampara/lampara';
+import { TvPage } from '../tv/tv';
+import { SmartPage } from '../smart/smart';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ bocina=BocinaPage;
+ lamp=LamparaPage;
+ tv=TvPage;
+ smart=SmartPage;
 
   constructor(public navCtrl: NavController) {
 
   }
-
+  bocinaa(){
+    this.navCtrl.push(this.bocina)
+  }
+  tvv(){
+    this.navCtrl.push(this.tv)
+  }
+  smartt(){
+    this.navCtrl.push(this.smart)
+  }
+  lampp(){
+    this.navCtrl.push(this.lamp)
+  }
 }
